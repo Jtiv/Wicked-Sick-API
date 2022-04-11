99c7540aeb8a728e2402015cb1c1a7531341292f
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-04-11 15:16:06 -0400

    redefined routes

--- routes/router.js
+++ routes/router.js
@@ -4,10 +4,10 @@ let controller = require('../controller.js');
 let router = new express.Router();
 
 // router.get('/', controller.get);
-router.post('/', controller.create);
+router.post('/incidents', controller.create);
 router.get('/incidents', controller.get);
-router.get('/:id', controller.getIncidentById);
-router.delete('/:id', controller.delete);
-router.put('/:id', controller.update);
+router.get('/incidents/:id', controller.getIncidentById);
+router.delete('/incidents/:id', controller.delete);
+router.put('/incidents/:id', controller.update);
 
 module.exports = router;
