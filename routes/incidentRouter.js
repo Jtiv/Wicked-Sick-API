a5727eb27efea53f39e4e2dd8b6c857fde525242
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-04-12 10:24:39 -0400

    added and installed cors -- enabling cors

--- routes/incidentRouter.js
+++ routes/incidentRouter.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let router = require('./router.js');
-
+let cors = require('cors');
 let app = express();
 
 let jsonParsingMiddleware = express.json();
+app.use(cors);
 app.use(jsonParsingMiddleware);
 app.use(router);
 
