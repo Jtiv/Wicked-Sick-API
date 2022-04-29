3e2afb3879d31139ecd11ebf64cb3f2a38257c46
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-04-29 11:02:04 -0400

    undid PORT commenting from debugging session

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const routes = require('./routes/incidentRouter.js');
 
 const app = routes;
 
-//const PORT = 8080;
+const PORT =  process.env.PORT || 8080;
 
 
 db.on("connected", () => {
   console.clear();
   console.log("Connecting to MongoDB!");
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, () => {
     console.log(
-      `Express server running in development on: http://localhost:${process.env.PORT}`
+      `Express server running in development on: http://localhost:${PORT}`
     );
   });
 });
 
 db.on('error', error => console.error("error connecting to server\n" + error));
-db.on('listening', () => console.log("yeah we really connected ze doublecheck" + process.env.PORT));
+db.on('listening', () => console.log("yeah we really connected ze doublecheck" + PORT));
